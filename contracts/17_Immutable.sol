6fead1fdbb2ec95f6408cbfd88a32d479435dc93
Huyxu2K <nguyenvanhuy2712000@gmail.com>
2023-08-01 15:56:48 +0700

    add constant

--- contracts/17_Immutable.sol
+++ contracts/17_Immutable.sol
@@ -1,19 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Imutable{
-    address  public immutable My_Address;
-    uint public  immutable My_Number;
-    constructor(){
-        My_Address=msg.sender;
-        My_Number=2712000;
+contract Imutable {
+    address public immutable My_Address;
+    uint public immutable My_Number;
+    address public constant My_Address1 = 0x0000000000000000000000000000000000000000;
+    uint public constant My_Number1 = 1112000;
+
+    constructor() {
+        My_Address = msg.sender;
+        My_Number = 2712000;
     }
 }
-
 //Error: immutable can't be modified afterwards.
-contract S is Imutable{
-   constructor(){
-      My_Address=0x0000000000000000000000000000000000000000;
-      My_Number=1112000;
-   }
-}
+// contract S is Imutable {
+//     constructor() {
+//         My_Address = 0x0000000000000000000000000000000000000000;
+//         My_Number = 1112000;
+//     }
+// }
